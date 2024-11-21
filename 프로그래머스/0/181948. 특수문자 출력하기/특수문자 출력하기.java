6ce6477e05142e9ac1bc9848b78e6deaa0c9bf74
Song Byeongmin <95574837+mini9707@users.noreply.github.com>
2024-11-21 19:46:25 +0900

    [level 0] Title: 특수문자 출력하기, Time: 115.36 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        
+        //JAVA 에서 \(백슬래시), '', "", 는 문자 그대로 출력되지 않음
+        //해당 특수문자 앞에 \를 써주면 자동으로 다음 문자를 인식
+    }
+}
